1e697e9b8933ffccbf4220d59bb4f0749263ef2a
Katrina Pritchard <pritckat@gmail.com>
2018-11-17 22:34:03 +0000

    automatically backed up by learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
+let state = {numberOfPresents: 0}
+
 export function managePresents(state, action){
+
+  switch(action.type) {
+
+    case "INCREASE":
+      return state;
+
+    default:
+      return state;
+  }
 }
